c8be22d421d18e00400de85f570e0de874503d9f
misha-solntcev <misha.solntcev@gmail.com>
2022-10-15 13:10:18 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Вариант 12
+Даны три целочисленных массива A(N), В(М) и C(L). Найдите хотя бы одно число,
+встречающееся во всех трех массивах. Если таких чисел нет, выведите соответствующее
+сообщение.*/
+
+namespace _4_1_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 5;
+            int[] A = new int[N] { 1, 2, 3, 5, 8 };
+            const int M = 7;
+            int[] B = new int[M] { 9, 10, 11, 12, 13, 14, 15 };
+            const int L = 3;
+            int[] C = new int[L] { 6, 7, 8, };
+
+            bool flag = false;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                    if (A[i] == B[j])
+                        for (int k = 0; k < L; k++)
+                            if (B[j] == C[k])
+                            {
+                                Console.WriteLine($"Найдено число {C[k]}");
+                                flag = true;
+                            }
+            }
+            if (!flag)
+                Console.WriteLine("Встречающихся 3 раза чисел нет.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("4_1_12")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("4_1_12")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b518640d-754f-46c6-bae6-b39e4fe33044")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
